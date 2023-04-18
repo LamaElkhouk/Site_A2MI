e3f5f18015078e590c8a6d79e7a8acbfdddd76af
LamaElkhouk <lamiaelkhoukhi09@gmail.com>
2023-04-18 18:14:29 +0200

    routage header

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -22,12 +22,91 @@ class DefaultController extends AbstractController
     {
         $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
 
-        $servicesActive = ($request->get('_route') === 'Services');
+        $Active = ($request->get('_route') === 'Services');
 
         return $this->render('default/index.html.twig', [
             'title' => 'Page accueil',
             'onglets' => $onglets,
-            'servicesActive' => $servicesActive
+            'Active' => $Active
+        ]);
+    }
+
+    //liens vers les services
+
+    #[Route('/Service_informatique', name: 'Service_informatique')]
+    public function service_informatique(Request $request): Response
+    {
+        $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
+
+        return $this->render('default/services/serviceInformatique.html.twig', [
+            'onglets' => $onglets
+        ]);
+    }
+
+    #[Route('/Communication', name: 'Communication')]
+    public function Communication(Request $request): Response
+    {
+        $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
+        return $this->render('default/services/communication.html.twig', [
+            'onglets' => $onglets
+        ]);
+    }
+
+    #[Route('/Assistance', name: 'Assistance')]
+    public function Assistance(Request $request): Response
+    {
+        $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
+        return $this->render('default/services/assistance.html.twig', [
+            'onglets' => $onglets
+        ]);
+    }
+
+    #[Route('/Formation', name: 'Formation')]
+    public function Formation(Request $request): Response
+    {
+        $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
+        return $this->render('default/services/formation.html.twig', [
+            'onglets' => $onglets
+        ]);
+    }
+
+
+    #[Route('/Gestion_administrative', name: 'Gestion_administrative')]
+    public function Gestion_administrative(Request $request): Response
+    {
+        $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
+        return $this->render('default/services/gestionAdministrative.html.twig', [
+            'onglets' => $onglets
+        ]);
+    }
+
+    #[Route('/Site_internet', name: 'Site_internet')]
+    public function Site_internet(Request $request): Response
+    {
+        $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
+        return $this->render('default/services/siteInternet.html.twig', [
+            'onglets' => $onglets
+        ]);
+    }
+
+    //lien vers la page agence
+
+    #[Route('/Agence', name: 'Agence')]
+    public function Agence(Request $request): Response
+    {
+        $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
+        return $this->render('default/agence.html.twig', [
+            'onglets' => $onglets
+        ]);
+    }
+
+    //lien vers la page mon compte
+    #[Route('/Mon_compte', name: 'Mon_compte')]
+    public function Mon_compte(Request $request): Response
+    {
+        $onglets = $this->ongletRepository->findAll(OngletsHeader::class);
+        return $this->render('default/monCompte.html.twig', [
+            'onglets' => $onglets
         ]);
     }
 }
